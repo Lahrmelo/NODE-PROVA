f49218a8b447e8bde3a10b01a613a0077684dd9c
Lara Rodrigues Melo <108811988+Lahrmelo@users.noreply.github.com>
2023-07-10 12:31:54 -0300

    Add files via upload

---
+++ prova-node/index.js
@@ -0,0 +1,17 @@
+//ATIVIDADE 1
+
+//const http = require('node:http');
+//const PORT = 2000;
+
+//const objeto = [
+    //{id:1, nome: 'windows'},
+    //{id:2, nome: '1245638'},
+    //{id:3, nome: 'users/3E'},
+//];
+
+//const json = JSON.stringify(objeto)
+//console.log(json) 
+  
+//-----------------------------------------------
+
+//ATIVIDADE 2
